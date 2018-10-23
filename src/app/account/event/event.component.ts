1afa6d265e89a527234faaa5984c754b6b3d1166
dakotah martin <dakotah@sidewalkpro.com>
2018-10-23 09:39:38 -0600

    added filterTypes to component, wouldn't build

--- src/app/account/event/event.component.ts
+++ src/app/account/event/event.component.ts
@@ -1,30 +1,29 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { trigger, style, transition, animate } from "@angular/animations";
-import { Timeclock, AccountService, Log, Event } from '../account.service';
-import { map, tap } from 'rxjs/operators';
-import * as moment from 'moment';
-import { MatDialog } from '@angular/material';
-import { ImagesDialogComponent } from '../images-dialog/images-dialog.component';
+import { Timeclock, AccountService, Log, Event } from "../account.service";
+import { map, tap } from "rxjs/operators";
+import * as moment from "moment";
+import { MatDialog } from "@angular/material";
+import { ImagesDialogComponent } from "../images-dialog/images-dialog.component";
 
 @Component({
-  selector: 'app-event',
-  templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css'],
+  selector: "app-event",
+  templateUrl: "./event.component.html",
+  styleUrls: ["./event.component.css"],
   animations: [
     trigger("expand", [
       transition("void => *", [
-        style({ height: 0}),
-        animate("400ms ease-in-out", style({ }))
+        style({ height: 0 }),
+        animate("400ms ease-in-out", style({}))
       ]),
       transition("* => void", [
-        style({ }),
+        style({}),
         animate("400ms ease-in-out", style({ height: 0 }))
       ])
     ])
   ]
 })
 export class EventComponent {
-
   searchStr; // template variable
   filterUsers; // template variable
 
@@ -33,49 +32,55 @@ export class EventComponent {
   days = [];
   eventTypes;
 
-  
+  filterTypes;
+
   lat: number;
   long: number;
 
-  now: any = moment().format('MMM');
+  now: any = moment().format("MMM");
 
-  constructor(
-    public accountService: AccountService,
-    public dialog: MatDialog
-  ) {
+  constructor(public accountService: AccountService, public dialog: MatDialog) {
     this.accountService.helper = this.accountService.helperProfiles.log;
     this.accountService.aTeamObservable.subscribe(aTeam => {
       if (aTeam) {
-        let eventCollection = this.accountService.db.collection("event", ref => ref.where("teamId", "==", this.accountService.aTeam.id))
-        eventCollection.snapshotChanges().pipe(
-          map(actions => {
-            return actions.map(a => {
-              let data:any = a.payload.doc.data();
-              return <Event>{
-                ...data,
-                id: a.payload.doc.id,
-                createdAt: data["createdAt"].toDate()
-              };
+        let eventCollection = this.accountService.db.collection("event", ref =>
+          ref.where("teamId", "==", this.accountService.aTeam.id)
+        );
+        eventCollection
+          .snapshotChanges()
+          .pipe(
+            map(actions => {
+              return actions.map(a => {
+                let data: any = a.payload.doc.data();
+                return <Event>{
+                  ...data,
+                  id: a.payload.doc.id,
+                  createdAt: data["createdAt"].toDate()
+                };
+              });
+            }),
+            tap(results => {
+              results.sort((a, b) => {
+                return a.createdAt < b.createdAt ? 1 : -1;
+              });
+            })
+          )
+          .subscribe(events => {
+            if (events.length == 0) this.accountService.showHelper = true;
+            this.events = events;
+            this.eventTypes = [];
+            this.events.forEach(event => {
+              if (this.oldestEvent > event.createdAt) {
+                this.oldestEvent = event.createdAt;
+              }
+              event.user = this.accountService.teamUsers.find(
+                user => user.id == event.userId
+              );
+              if (!this.eventTypes.find(type => type == event.type))
+                this.eventTypes.push(event.type);
             });
-          }),
-          tap(results => {
-            results.sort((a, b) => {
-              return a.createdAt < b.createdAt ? 1 : -1;
-            });
-          })
-        ).subscribe(events => {
-          if (events.length == 0) this.accountService.showHelper = true;
-          this.events = events;
-          this.eventTypes = [];
-          this.events.forEach(event => {
-            if (this.oldestEvent > event.createdAt) {
-              this.oldestEvent = event.createdAt;
-            }
-            event.user = this.accountService.teamUsers.find(user => user.id == event.userId);
-            if (!this.eventTypes.find(type => type == event.type)) this.eventTypes.push(event.type);
+            this.buildCalendar();
           });
-          this.buildCalendar();
-        }); 
       }
     });
   }
@@ -83,20 +88,35 @@ export class EventComponent {
   buildCalendar() {
     this.days = [];
     let now: any = new Date();
-    let total_days = Math.round((now - this.oldestEvent) / (1000 * 60 * 60 * 24)); //moment equilivant?
+    let total_days = Math.round(
+      (now - this.oldestEvent) / (1000 * 60 * 60 * 24)
+    ); //moment equilivant?
     for (let i = 0; i <= total_days; i++) {
-      let date = moment().subtract(i, 'days');
-      let month = date.format('MMM');
-      let displayMonth = moment().subtract(i, 'days').subtract(1, 'month').format("MMM");
-      let day = date.format('DD');
-      let dOW = date.format('ddd');
-      let events = this.getEventsByDate(date); 
-      this.days.push({id: i + 1, date, month, displayMonth, day, dOW, events: events.events});
+      let date = moment().subtract(i, "days");
+      let month = date.format("MMM");
+      let displayMonth = moment()
+        .subtract(i, "days")
+        .subtract(1, "month")
+        .format("MMM");
+      let day = date.format("DD");
+      let dOW = date.format("ddd");
+      let events = this.getEventsByDate(date);
+      this.days.push({
+        id: i + 1,
+        date,
+        month,
+        displayMonth,
+        day,
+        dOW,
+        events: events.events
+      });
     }
   }
 
   getEventsByDate(date) {
-    let eventsOnDate: Event[] = this.events.filter(event => moment(event.createdAt).isSame(date, 'day'));
+    let eventsOnDate: Event[] = this.events.filter(event =>
+      moment(event.createdAt).isSame(date, "day")
+    );
     return { events: eventsOnDate };
   }
 
@@ -104,10 +124,14 @@ export class EventComponent {
     if (event.details) {
       event.bShowDetails = true;
     } else {
-      let doc = this.accountService.db.collection(event.type).doc(event.documentId);
+      let doc = this.accountService.db
+        .collection(event.type)
+        .doc(event.documentId);
       doc.valueChanges().subscribe((details: any) => {
-        details.clockIn ? details.clockIn = details.clockIn.toDate() : null;
-        details.clockOut ? details.clockOut = details.clockOut.toDate() : null;
+        details.clockIn ? (details.clockIn = details.clockIn.toDate()) : null;
+        details.clockOut
+          ? (details.clockOut = details.clockOut.toDate())
+          : null;
         event.details = details;
         event.bShowDetails = true;
       });
@@ -119,18 +143,15 @@ export class EventComponent {
     let co = moment(time.clockOut);
     let duration: any = moment.duration(co.diff(ci));
     let lh = parseInt(duration.asHours());
-    let lm = parseInt(duration.asMinutes())%60;
-    return lh + 'h ' + lm + 'm';
+    let lm = parseInt(duration.asMinutes()) % 60;
+    return lh + "h " + lm + "m";
   }
 
   showImages(images) {
     let dialog = this.dialog.open(ImagesDialogComponent, {
       data: images
-    })
+    });
   }
 
-  export() {
-    
-  }
-
+  export() {}
 }
