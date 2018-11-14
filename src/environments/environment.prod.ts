4cd490ba2c0de27126f43dde7759df84da12ddbc
jaden <jaden210@gmail.com>
2018-11-14 15:52:31 -0700

    prod for stripe

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,9 @@ export const environment = {
     projectId: "teamlog-2d74c",
     storageBucket: "teamlog-2d74c.appspot.com",
     messagingSenderId: "800172058622"
+  },
+  stripe: {
+    publishable: "pk_test_xQTLf7GJIEhTX1tz1AVeJaea",
+    secret: "sk_test_eBRntjsgqdxvMjQptP3TnZo2"
   }
 };
