0faa970e1a5dde4d69fbebf6b30457d5b217c36f
jaden <jaden210@gmail.com>
2018-12-29 10:52:56 -0500

    homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,30 @@ import { AppService } from '../app.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public appService: AppService) { }
+  loginErrorStr;
+  email;
+
+  constructor(public appService: AppService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  createAccount(): void {
+    this.loginErrorStr = !this.email ? "email required" : null;
+    if (!this.loginErrorStr) {
+      this.appService.email = this.email;
+      this.appService.checkForExistingUser(this.email).then(
+        isExistingUser => {
+          if (!isExistingUser)
+            this.appService.getInvites(this.email).subscribe(invites => {
+              if (invites.length > 0) this.router.navigate(["/join-team"]);
+              else this.router.navigate(["/get-started"]);
+            });
+          else this.router.navigate(["/sign-in"]);
+        },
+        error => (this.loginErrorStr = error)
+      );
+    }
   }
 
 }
